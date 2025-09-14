2bbf9c255e595ca2675b0238ffa6253147cf8750
mariovirglinio <mariovirglinio@gmail.com>
2025-09-14 11:55:49 -0300

    Add files via upload

---
+++ examples/BasicUsage/BasicUsage.ino
@@ -0,0 +1,17 @@
+#include <SoftTimer.h>
+
+tTimerTime ledEvent;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  initTimer();
+  setTimerEvent(&ledEvent, 1000); // 1 segundo
+}
+
+void loop() {
+  if (TimerHasExpired(&ledEvent)) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // toggle LED
+    setTimerEvent(&ledEvent, 1000); // reprogramar
+  }
+}
+
---
+++ src/soft_timer.h
@@ -0,0 +1,27 @@
+#ifndef SOFT_TIMER_H
+#define SOFT_TIMER_H
+
+#include <Arduino.h>
+
+typedef unsigned long dword;
+
+/* Estructura de un evento temporizado */
+typedef struct {
+  dword Start;
+  dword Expiration;
+} tTimerTime;
+
+/* Inicializa el sistema de timers (opcional en Arduino) */
+void initTimer();
+
+/* Programa un evento con duración en milisegundos */
+void setTimerEvent(tTimerTime *event, dword miliSeconds);
+
+/* Devuelve true si el evento ha expirado */
+bool TimerHasExpired(tTimerTime *event);
+
+/* Devuelve el tiempo actual en ms desde el arranque */
+dword getMillis();
+
+#endif
+
